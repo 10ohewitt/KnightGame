a2e92c0778328435e3d6582f494bb3bddc9705ba
10ohewitt <143799558+10ohewitt@users.noreply.github.com>
2024-01-09 11:51:01 +0000

    ...

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    private Animator _anim;
+    public Animator _anim;
+    public Renderer ren;
     void Start()
     {
+        ren.GetComponent<Renderer>();
+        ren.enabled = false;
         _anim = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         Attack();
@@ -19,10 +21,23 @@ public class PlayerAttack : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         { 
-            if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Attack01") == false)
+            if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Idle_Battle") ||
+                _anim.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.WalkForwardBattle"))
             {
-                _anim.Play("Base Layer.Attack01", 0, 0f);
+                _anim.Play("Base Layer.Attack02", 0, 0f);
+                Invoke("Show", 0.3f);
+                Invoke("Hide", 0.55f);
             }
         }
+    }
+
+    void Hide()
+    {
+        ren.enabled = false;
+    }
+
+    void Show()
+    {
+        ren.enabled = true;
     }
 }
