d68c7b44d476a7c54aba98025050c786aa4ce54e
10ohewitt <143799558+10ohewitt@users.noreply.github.com>
2024-01-11 10:15:45 +0000

    ...

---
+++ Assets/GUIScripts/StartButton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string clickedButton = null;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class TopDownCharacterMover : MonoBehaviour
+    {
+        public Animator anim;
+        [SerializeField] float moveSpeed = 4f;
+        Vector3 forward, right;
+        public GameObject canvas;
+    
+        void Update ()
+        {
+            if (canvas.activeInHierarchy == false)
+            {
+                if (Input.GetAxisRaw("Horizontal") == 0f && Input.GetAxisRaw("Vertical") == 0f)
+                {
+                    anim.SetBool("Walk", false);
+                }
+                else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Idle_Battle"))
+                {
+                    anim.SetBool("Walk", true);
+                    anim.Play("WalkForwardBattle");
+                }
+
+                if (anim.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Idle_Battle") ||
+                    anim.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.WalkForwardBattle"))
+                {
+                    anim = GetComponent<Animator>();
+                    forward = Camera.main.transform.forward;
+                    forward.y = 0;
+                    forward = Vector3.Normalize(forward);
+                    right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+
+                    Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+                    if (direction.magnitude > 0.1f)
+                    {
+                        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxisRaw("Horizontal");
+                        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical");
+
+                        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+                        transform.forward = heading;
+                        transform.position += heading * moveSpeed * Time.deltaTime;
+                    }
+                }
+            }
+        }
+    }
+}
