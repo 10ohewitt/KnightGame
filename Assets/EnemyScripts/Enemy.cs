4fb6da5e6e8d493745447d38c81e91073d30bee2
10ohewitt <143799558+10ohewitt@users.noreply.github.com>
2024-01-17 15:16:48 +0000

    ...

--- Assets/EnemyScripts/Enemy.cs
+++ Assets/EnemyScripts/Enemy.cs
@@ -85,10 +85,6 @@ public class Enemy : MonoBehaviour
         }
     }
 
-
-
-
-
     public void HitVFX(Vector3 hitPosition)
     {
         GameObject hit = Instantiate(hitVFX, hitPosition, Quaternion.identity);
@@ -102,4 +98,35 @@ public class Enemy : MonoBehaviour
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, aggroRange);
     }
+
+
+
+
+    //I first start getting the transform (position) of my player
+    public Transform target;
+    //Then I set up the speed of the enemy, that I can edit later
+    public float speed = 2f;
+    //Lastly, I added the enemy a rigidbody
+    public Rigidbody rb;
+
+    //First thing, I will create a function that follows the player
+    void FollowPlayer()
+    {
+        //I will create a vector 3 called pos that stores the movement that I want my player to do
+        Vector3 pos = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        //I will use these two built-in functions to follow the player
+        rb.MovePosition(pos);
+        transform.LookAt(target);
+    }
+
+    //Finally, I add a collider function that calls the FollowPlayer() function when it is within its range
+    void OnCollisionExit(Collider player)
+    {
+        if (player.tag == "Player")
+        {
+            FollowPlayer();
+        }
+    }
+
+   
 }
